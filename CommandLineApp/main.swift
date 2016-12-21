d3c6a2adfcf4e43ec378034f858362df7f5014ad
wedgwoodtom <wedgwood.tom@gmail.com>
2016-12-21 15:36:15 -0800

    Initial Commit

---
+++ CommandLineApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CommandLineApp
+//
+//  Created by Thomas Patterson on 12/21/16.
+//  Copyright © 2016 Thomas Patterson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
